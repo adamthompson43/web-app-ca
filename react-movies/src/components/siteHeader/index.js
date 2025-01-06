8de93937c9b70f5718bb0d32aff75a297f11bb8c
Adam T <aovert.at@gmail.com>
2025-01-06 06:56:58 +0000

    edited site header to only show favourites and playlist routes to logged in users

--- react-movies/src/components/siteHeader/index.js
+++ react-movies/src/components/siteHeader/index.js
@@ -28,11 +28,13 @@ const SiteHeader = ({ history }) => {
 
   const menuOptions = [
     { label: "Home", path: "/" },
-    { label: "Favorites", path: "/movies/favorites" },
     { label: "Popular", path: "/movies/popular"},
     { label: "Upcoming", path: "/movies/upcoming" },
     { label: "Top Rated", path: "/movies/topRated"},
+    ...(context.isAuthenticated ? [
+    { label: "Favorites", path: "/movies/favorites" },
     { label: "Playlist", path: "/movies/playlist" }
+    ] : [])
   ];
 
   const handleMenuSelect = (pageURL) => {
