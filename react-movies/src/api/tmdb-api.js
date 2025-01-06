9a4eed42f70890482223ee69d43557303e9166fa
Adam T <aovert.at@gmail.com>
2025-01-06 01:15:28 +0000

    added login and signup to the api, reused authcontext.js from labs

--- react-movies/src/api/tmdb-api.js
+++ react-movies/src/api/tmdb-api.js
@@ -133,6 +133,34 @@ export const getMovie = (args) => {
       throw error;
     });
   };
+
+  export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users',
+      { 
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "post",
+        body: JSON.stringify({ username: username, password })
+
+      }
+    );
+    return response.json();
+  }
+
+  export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users/?action=register',
+      { 
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "post",
+        body: JSON.stringify({ username: username, password })
+
+      }
+    );
+    return response.json();
+  }
   
   
   
